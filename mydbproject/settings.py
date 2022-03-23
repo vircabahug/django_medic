c06c2677c3cc322c036a2b5193cb7daf91b2a56c
vircabahug <vir.cabahug1@gmail.com>
2022-03-23 12:04:45 +0800

    hosts

--- mydbproject/settings.py
+++ mydbproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-qn8(zvw3v9twgc3dy65f5$g7o^28$i3k0+z067b+58^)b*b4$_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
